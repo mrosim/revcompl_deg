cbf074b86c33373deee9d7ef034a16ffb1b845b0
msim <56232384+mrosim@users.noreply.github.com>
2021-05-20 22:39:10 +0200

    Update rev_compl_deg.py

--- rev_compl_deg.py
+++ rev_compl_deg.py
@@ -4,9 +4,6 @@ def rev_compl(seq):
     BASES ='NACRWMBDHVKSYGT'
     return ''.join([BASES[-j] for j in [BASES.find(i) for i in seq.replace('-','')][::-1]])
 
-def main():
+if __name__ == '__main__':
     inseq = input('sequence: ')
     print(rev_compl(inseq.upper()))
-
-if __name__ == '__main__':
-    main()
